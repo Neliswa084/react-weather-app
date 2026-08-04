1aa3e23fe726f791afe186e6b61c2aea61173f3b
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-05 00:04:24 +0200

    Navigation from search to weather page and also from weather page to search page

---
+++ src/Components/CurrentWeather/CurrentWeather.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Text} from '../../Components/Text/Text'
+import styles from './CurrentWeather.module.css'
+import {NavLink ,useNavigate} from 'react-router-dom'
+import backIcon from '../../assets/back.png'
+
+export const CurrentWeather = () => {
+  return (
+    <div>
+         <NavLink to={'/'} className={styles.link} >
+         <img className={styles['icon']} src={backIcon} alt="Back" />
+         Back to Search</NavLink>
+        <Text variant={'h2'}>Pitermarizburg</Text>
+        <Text variant={'p'}>Tuesday , 04 August</Text>
+        <Text variant={'h1'}>25°C</Text>
+        <Text variant={'p'}>Partly Cloudy</Text>
+
+    </div>
+  )
+}
--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { CurrentWeather } from '../Components/CurrentWeather/CurrentWeather'
 
 export const WeatherPage = () => {
   return (
-    <div>WeatherPage</div>
+    <CurrentWeather />
   )
 }
