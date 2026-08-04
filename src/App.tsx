28c17e77c93cb82828be95c60dee37cd1273f2ab
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-04 10:41:13 +0200

    Search component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Searchbar } from './Components/Search/Searchbar'
 
 function App() {
 
 
   return (
     <>
+    <Searchbar />
     
     </>
   )
---
+++ src/Components/Search/Searchbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Text} from '../../Components/Text/Text'
+import styles from './Searchbar.module.css'
+import searchIcon from '../../assets/searchIcon.png'
+
+export const Searchbar = () => {
+  return (
+      <div className={styles['search-bar']}>
+      <Text variant={'span'} style={{color: 'rgb(20,20,20)', padding: 10}}>Search</Text>
+      <input 
+        type='text' 
+        placeholder='search by title, URL, description or tag' 
+        className={styles['search-input']}
+      
+      />
+      <img src={searchIcon} alt='search icon' className={styles['search-icon']} />
+    </div>
+   
+  )
+}
